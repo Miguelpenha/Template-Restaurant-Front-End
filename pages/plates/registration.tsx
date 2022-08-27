04d4e382cbba2f687c6a369bcfae8df8ab886dca
Miguel Penha <miguel.penha1@gmail.com>
2022-08-26 23:44:50 -0300

    Ajeitando algumas coisas

--- pages/plates/registration.tsx
+++ pages/plates/registration.tsx
@@ -1,7 +1,17 @@
-import { Container, HeaderNav, ButtonBack, IconButtonBack, Title } from '../../styles/pages/plates/registration'
+import { Container, HeaderNav, ButtonBack, IconButtonBack, Title, Form, Field, Label, Input, InputTextArea, ButtonSubmit } from '../../styles/pages/plates/registration'
 import Link from 'next/link'
+import { useState } from 'react'
+import api from '../../api/base'
+import { useRouter } from 'next/router'
 
 function Registration() {
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('R$ 0,00')
+    const [weight, setWeight] = useState(100)
+    const [peoplesCount, setPeoplesCount] = useState(1)
+    const [description, setDescription] = useState('')
+    const router = useRouter()
+
     return (
         <Container>
             <HeaderNav>
@@ -15,6 +25,41 @@ function Registration() {
                 </Link>
                 <Title>Cadastrar Prato</Title>
             </HeaderNav>
+            <Form onSubmit={async ev => {
+                ev.preventDefault()
+
+                await api.post('/plates', {
+                    name,
+                    price,
+                    weight,
+                    peoplesCount,
+                    description
+                })
+
+                router.back()
+            }}>
+                <Field>
+                    <Label htmlFor="Nome">Nome</Label>
+                    <Input type="text" onChange={ev => setName(ev.target.value)} defaultValue={name} id="name" name="name" placeholder="Nome do prato..."/>
+                </Field>
+                <Field>
+                    <Label htmlFor="Preço">Preço</Label>
+                    <Input type="text" onChange={ev => setPrice(ev.target.value)} defaultValue={price} id="price" name="price" placeholder="Preço do prato..."/>
+                </Field>
+                <Field>
+                    <Label htmlFor="Peso">Peso</Label>
+                    <Input type="number" onChange={ev => setWeight(Number(ev.target.value))} defaultValue={weight} step="10" id="weight" name="weight" placeholder="Peso do prato..."/>
+                </Field>
+                <Field>
+                    <Label htmlFor="Serve quantas pessoas">Serve quantas pessoas</Label>
+                    <Input type="number" onChange={ev => setPeoplesCount(Number(ev.target.value))} defaultValue={peoplesCount} id="peoplesCount" name="peoplesCount" placeholder="Serve quantas pessoas..."/>
+                </Field>
+                <Field>
+                    <Label htmlFor="Descrição">Descrição</Label>
+                    <InputTextArea rows={3} onChange={ev => setDescription(ev.target.value)} defaultValue={description} id="description" name="description" placeholder="Descrição do prato..."/>
+                </Field>
+                <ButtonSubmit type="submit">Cadastrar</ButtonSubmit>
+            </Form>
         </Container>
     )
 }
--- styles/pages/plates/registration.ts
+++ styles/pages/plates/registration.ts
@@ -39,3 +39,84 @@ export const Title = styled.h1`
     font-size: 2.5vw;
     color: ${props => props.theme.primary};
 `
+
+export const Form = styled.form`
+    width: 30%;
+    padding: 2%;
+    display: flex;
+    margin-top: 2%;
+    margin-bottom: 5%;
+    align-self: center;
+    border-radius: 15px;
+    flex-direction: column;
+    background-color: ${props => props.theme.backgroundColorSecondary};
+`
+
+export const Field = styled.div`
+    width: 80%;
+    display: flex;
+    margin-bottom: 4%;
+    align-self: center;
+    flex-direction: column;
+`
+
+export const Label = styled.label`
+    
+`
+
+export const Input = styled.input`
+    padding: 4%;
+    border: none;
+    font-size: 1vw;
+    margin-top: 4%;
+    border-radius: 5px;
+    color: ${props => props.theme.color};
+    background-color: ${props => props.theme.backgroundColor};
+    
+    :focus {
+        outline: none;
+    }
+`
+
+export const InputTextArea = styled.textarea`
+    padding: 4%;
+    border: none;
+    font-size: 1vw;
+    margin-top: 4%;
+    resize: vertical;
+    border-radius: 5px;
+    color: ${props => props.theme.color};
+    background-color: ${props => props.theme.backgroundColor};
+    
+    :focus {
+        outline: none;
+    }
+`
+
+export const ButtonSubmit = styled.button`
+    width: 80%;
+    padding: 4%;
+    border: none;
+    display: flex;
+    margin-top: 5%;
+    cursor: pointer;
+    font-size: 1.5vw;
+    font-weight: bold;
+    align-self: center;
+    align-items: center;
+    border-radius: 15px;
+    text-decoration: none;
+    transform: scale(0.95);
+    transition-duration: 0.1s;
+    justify-content: space-evenly;
+    transition-timing-function: linear;
+    color: ${props => props.theme.primary};
+    box-shadow: rgba(0, 0, 0, 0.3) 0px 4px 8px 0px;
+    background-color: ${props => props.theme.backgroundColor};
+
+    :hover {
+        opacity: 0.8;
+        transform: scale(1);
+        box-shadow: rgba(0, 0, 0, 0.3) 0px 6px 15px 0px;
+    }
+`
